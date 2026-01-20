159809ba4b91e2193c8af5faadd357fe5a24f50b
Miguel Tamat <mtamat-2024024@kinal.edu.gt>
2026-01-20 11:19:09 -0600

    Se agrego este archivo

--- Agenda/src/components/sections/tareas/stoge.js
+++ Agenda/src/components/sections/tareas/stoge.js
@@ -1,13 +1,8 @@
 let LOCAL_STORAGE_KEY = "tareas";
 
 let TareaList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [
-    {
-        estado: false,
-        nombre: "Comprar Aliminetos",
-        pioridad: "Alta"
-    }
+    { estado: false, nombre: "Comprar Alimentos", prioridad: "Alta" }
 ];
-
 
 let guardarTarea = () => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(TareaList));
